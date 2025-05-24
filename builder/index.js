3de9883d443c7b48c1ff62e8cac0d2b884001181
Pietro Teruya <pietrodomingues@gmail.com>
2025-05-24 15:38:08 -0300

    re-organizing folders and fixing pipeline

--- builder/index.js
+++ builder/index.js
@@ -8,6 +8,16 @@ fs.mkdirSync('../compiled', { recursive: true });
 /// Move the index.html file to the compiled folder
 fs.copyFileSync('../blog/structure/index.html', '../compiled/index.html');
 
+/// Copy the assets folder to the compiled folder
+fs.cpSync('../blog/structure/assets', '../compiled/assets', {
+    recursive: true
+});
+
+/// Copy the styles folder to the compiled folder
+fs.cpSync('../blog/structure/styles', '../compiled/styles', {
+    recursive: true
+});
+
 // /// List blog files
 // var files = fs.readdirSync('../blog/posts');
 
