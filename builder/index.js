58ae47f9433f58b93963e8c51f48d1655829bd57
Pietro Teruya <pietrodomingues@gmail.com>
2025-05-24 15:32:59 -0300

    preparing for github pipeline

--- builder/index.js
+++ builder/index.js
@@ -2,25 +2,31 @@ import fs from 'fs';
 import path from 'path';
 import { processBlogPost } from './processor.js';
 
-/// List blog files
-var files = fs.readdirSync('../blog/posts');
+/// Create the `compiled` folder
+fs.mkdirSync('../compiled', { recursive: true });
 
-/// Create the `compiled/posts` folder
-fs.mkdirSync('../compiled/posts', { recursive: true });
+/// Move the index.html file to the compiled folder
+fs.copyFileSync('../blog/structure/index.html', '../compiled/index.html');
+
+// /// List blog files
+// var files = fs.readdirSync('../blog/posts');
+
+// /// Create the `compiled/posts` folder
+// fs.mkdirSync('../compiled/posts', { recursive: true });
 
 /// Read, process and write compiled file for each one of the MDX posts
-for (var file of files){
-    /// Read the file content
-    var content =
-            fs.readFileSync(path.join('../blog/posts', file), 'utf8');
+// for (var file of files) {
+//     /// Read the file content
+//     var content =
+//         fs.readFileSync(path.join('../blog/posts', file), 'utf8');
 
-    /// Process the content        
-    var processedContent = await processBlogPost(content);
+//     /// Process the content        
+//     var processedContent = await processBlogPost(content);
 
-    /// Write the processed file to compiled folder
-    var filename = file.split('.')[0];
-    var renderedFilename = filename + '.html';
-    fs.writeFileSync(path.join('../compiled/posts', renderedFilename), processedContent);       
-}
+//     /// Write the processed file to compiled folder
+//     var filename = file.split('.')[0];
+//     var renderedFilename = filename + '.html';
+//     fs.writeFileSync(path.join('../compiled/posts', renderedFilename), processedContent);
+// }
 
 
